c1e93910827f10e8afc6f24ef34ebdda2e8f3b32
Scar Wu <xneriscool@gmail.com>
2022-06-05 16:29:56 +0800

    1. update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-'use strict';
 /**
  * Gulp Tasks
  *
@@ -6,24 +5,24 @@
  * @copyright   Copyright (c) Scar Wu (http://scar.tw)
  */
 
-let gulp = require('gulp');
-let del = require('del');
-let $ = require('gulp-load-plugins')();
-let log = require('fancy-log');
-let colors = require('ansi-colors');
-let webpack = require('webpack');
-let webpackStream = require('webpack-stream');
-let webpackConfig = require('./webpack.config.js');
-let postfix = (new Date()).getTime().toString();
+let gulp = require('gulp')
+let del = require('del')
+let $ = require('gulp-load-plugins')()
+let log = require('fancy-log')
+let colors = require('ansi-colors')
+let webpack = require('webpack')
+let webpackStream = require('webpack-stream')
+let webpackConfig = require('./webpack.config.js')
+let postfix = (new Date()).getTime().toString()
 
-let ENVIRONMENT = 'development';
-let WEBPACK_NEED_WATCH = false;
+let ENVIRONMENT = 'development'
+let WEBPACK_NEED_WATCH = false
 
 /**
  * Compile Style & Script
  */
 function handleCompileError(event) {
-    log.error(colors.red(event.message), 'error.');
+    log.error(colors.red(event.message), 'error.')
 }
 
 function compileSass() {
@@ -33,10 +32,10 @@ function compileSass() {
         }).on('error', handleCompileError))
         .pipe($.autoprefixer())
         .pipe($.rename(function (path) {
-            path.basename = path.basename.split('.')[0];
-            path.extname = '.min.css';
+            path.basename = path.basename.split('.')[0]
+            path.extname = '.min.css'
         }))
-        .pipe(gulp.dest('temp/assets/styles'));
+        .pipe(gulp.dest('temp/assets/styles'))
 }
 
 function compileWebpack(callback) {
@@ -47,25 +46,25 @@ function compileWebpack(callback) {
                 'BUILD_TIME': postfix,
                 'NODE_ENV': JSON.stringify('production')
             }
-        });
+        })
 
-        webpackConfig.mode = ENVIRONMENT;
-        webpackConfig.plugins = webpackConfig.plugins || [];
-        webpackConfig.plugins.push(definePlugin);
+        webpackConfig.mode = ENVIRONMENT
+        webpackConfig.plugins = webpackConfig.plugins || []
+        webpackConfig.plugins.push(definePlugin)
     }
 
     if (WEBPACK_NEED_WATCH) {
-        webpackConfig.watch = true;
+        webpackConfig.watch = true
     }
 
     let result = gulp.src('src/assets/scripts/theme.js')
         .pipe(webpackStream(webpackConfig, webpack).on('error', handleCompileError))
-        .pipe(gulp.dest('temp/assets/scripts'));
+        .pipe(gulp.dest('temp/assets/scripts'))
 
     if (WEBPACK_NEED_WATCH) {
-        callback();
+        callback()
     } else {
-        return result;
+        return result
     }
 }
 
@@ -77,37 +76,37 @@ function copyMeta() {
             'src/constant.php',
             'src/config.php'
         ])
-        .pipe(gulp.dest('temp'));
+        .pipe(gulp.dest('temp'))
 }
 
 function copyExtensions() {
     return gulp.src('src/extensions/**/*')
-        .pipe(gulp.dest('temp/extensions'));
+        .pipe(gulp.dest('temp/extensions'))
 }
 
 function copyHandlers() {
     return gulp.src('src/handlers/**/*')
-        .pipe(gulp.dest('temp/handlers'));
+        .pipe(gulp.dest('temp/handlers'))
 }
 
 function copyViews() {
     return gulp.src('src/views/**/*')
-        .pipe(gulp.dest('temp/views'));
+        .pipe(gulp.dest('temp/views'))
 }
 
 function copyAssetsFonts() {
     return gulp.src('src/assets/fonts/*')
-        .pipe(gulp.dest('temp/assets/fonts'));
+        .pipe(gulp.dest('temp/assets/fonts'))
 }
 
 function copyAssetsImages() {
     return gulp.src('src/assets/images/**/*')
-        .pipe(gulp.dest('temp/assets/images'));
+        .pipe(gulp.dest('temp/assets/images'))
 }
 
 function copyVendorFonts() {
     return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*.{otf,eot,svg,ttf,woff,woff2}')
-        .pipe(gulp.dest('temp/assets/fonts/vendor'));
+        .pipe(gulp.dest('temp/assets/fonts/vendor'))
 }
 
 /**
@@ -116,17 +115,19 @@ function copyVendorFonts() {
 function watch() {
 
     // Watch Files
-    gulp.watch('temp/**/*').on('change', $.livereload.changed);
-    gulp.watch('src/{config,constant}.php', copyMeta);
-    gulp.watch('src/extensions/**/*', copyExtensions);
-    gulp.watch('src/handlers/**/*', copyHandlers);
-    gulp.watch('src/views/**/*', copyViews);
-    gulp.watch('src/assets/fonts/*', copyAssetsFonts);
-    gulp.watch('src/assets/images/**/*', copyAssetsImages);
-    gulp.watch('src/assets/styles/**/*.{sass,scss}', compileSass);
+    gulp.watch('temp/**/*').on('change', $.livereload.changed)
+    gulp.watch('src/{config,constant}.php', copyMeta)
+    gulp.watch('src/extensions/**/*', copyExtensions)
+    gulp.watch('src/handlers/**/*', copyHandlers)
+    gulp.watch('src/views/**/*', copyViews)
+    gulp.watch('src/assets/fonts/*', copyAssetsFonts)
+    gulp.watch('src/assets/images/**/*', copyAssetsImages)
+    gulp.watch('src/assets/styles/**/*.{sass,scss}', compileSass)
 
     // Start LiveReload
-    $.livereload.listen();
+    $.livereload.listen({
+        host: '0.0.0.0'
+    })
 }
 
 /**
@@ -134,13 +135,13 @@ function watch() {
  */
 function releaseCopyAll() {
     return gulp.src('temp/**/*')
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'))
 }
 
 function releaseReplaceIndex() {
     return gulp.src('dist/views/index.php')
         .pipe($.replace('$postfix = time()', '$postfix = ' + postfix))
-        .pipe(gulp.dest('dist/views'));
+        .pipe(gulp.dest('dist/views'))
 }
 
 /**
@@ -149,28 +150,28 @@ function releaseReplaceIndex() {
 function setEnv(callback) {
 
     // Warrning: Change ENVIRONMENT to Prodctuion
-    ENVIRONMENT = 'production';
+    ENVIRONMENT = 'production'
 
-    callback();
+    callback()
 }
 
 function setWatch(callback) {
 
     // Webpack need watch
-    WEBPACK_NEED_WATCH = true;
+    WEBPACK_NEED_WATCH = true
 
-    callback();
+    callback()
 }
 
 /**
  * Clean Temp Folders
  */
 function cleanTemp() {
-    return del('temp');
+    return del('temp')
 }
 
 function cleanDist() {
-    return del('dist');
+    return del('dist')
 }
 
 /**
@@ -180,16 +181,16 @@ gulp.task('prepare', gulp.series(
     cleanTemp,
     gulp.parallel(copyMeta, copyExtensions, copyHandlers, copyViews, copyAssetsFonts, copyAssetsImages, copyVendorFonts),
     gulp.parallel(compileSass, compileWebpack)
-));
+))
 
 gulp.task('release', gulp.series(
     setEnv, cleanDist,
     'prepare',
     releaseCopyAll, releaseReplaceIndex
-));
+))
 
 gulp.task('default', gulp.series(
     setWatch,
     'prepare',
     watch
-));
+))
